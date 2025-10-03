32788bb4a4c354dead7383641c6d200da80208fe
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2025-10-03 13:31:00 +0100

    1.21.9

--- common/src/main/java/cc/barnab/commandoptimiser/mixin/BaseCommandBlockMixin.java
+++ common/src/main/java/cc/barnab/commandoptimiser/mixin/BaseCommandBlockMixin.java
@@ -2,11 +2,14 @@ package cc.barnab.commandoptimiser.mixin;
 
 import com.mojang.brigadier.ParseResults;
 import net.minecraft.commands.CommandResultCallback;
+import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.BaseCommandBlock;
+import net.minecraft.world.level.BaseCommandBlock.CloseableCommandBlockSource;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -14,6 +17,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
+
+import static net.minecraft.commands.Commands.trimOptionalPrefix;
 
 @Mixin(BaseCommandBlock.class)
 public abstract class BaseCommandBlockMixin {
@@ -26,33 +33,48 @@ public abstract class BaseCommandBlockMixin {
     @Unique
     private String commandoptimiser$commandNoSlash;
 
-    @Shadow public abstract CommandSourceStack createCommandSourceStack();
+    @Shadow public abstract CommandSourceStack createCommandSourceStack(CommandSource commandSource);
 
     @Shadow private int successCount;
+
+    @Shadow @Nullable protected abstract CloseableCommandBlockSource createSource();
+
     @Unique
     private ParseResults<CommandSourceStack> commandoptimiser$parseResults;
 
     @Inject(method = "setCommand", at = @At("TAIL"))
-    private void parseOnSetCommand(String string, CallbackInfo ci) {
+    private void parseOnSetCommand(String string, CallbackInfo ci) throws Exception {
         // Parse when the player changes the command
         if (getLevel() != null)
             commandoptimiser$parse();
     }
 
-    @Redirect(method = "performCommand", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/BaseCommandBlock;createCommandSourceStack()Lnet/minecraft/commands/CommandSourceStack;"))
-    private CommandSourceStack skipCommandSource(BaseCommandBlock instance) {
+    @Redirect(method = "performCommand", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/BaseCommandBlock;createSource()Lnet/minecraft/world/level/BaseCommandBlock$CloseableCommandBlockSource;"))
+    private CloseableCommandBlockSource skipCommandSource(BaseCommandBlock instance) {
+        // Don't create unused command source
+        return null;
+    }
+
+    @Redirect(method = "performCommand", at = @At(value = "INVOKE", target = "Ljava/util/Objects;requireNonNullElse(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"))
+    private Object skipCommandSource2(Object obj, Object defaultObj) {
+        // Don't create unused command source
+        return null;
+    }
+
+    @Redirect(method = "performCommand", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/BaseCommandBlock;createCommandSourceStack(Lnet/minecraft/commands/CommandSource;)Lnet/minecraft/commands/CommandSourceStack;"))
+    private CommandSourceStack skipCommandSource3(BaseCommandBlock instance, CommandSource commandSource) {
         // Don't create unused command source
         return null;
     }
 
     @Redirect(method = "performCommand", at = @At(value = "INVOKE", target = "Lnet/minecraft/commands/CommandSourceStack;withCallback(Lnet/minecraft/commands/CommandResultCallback;)Lnet/minecraft/commands/CommandSourceStack;"))
-    private CommandSourceStack skipCommandSource2(CommandSourceStack instance, CommandResultCallback commandResultCallback) {
+    private CommandSourceStack skipCommandSource4(CommandSourceStack instance, CommandResultCallback commandResultCallback) {
         // Don't call withCallback on null command source
         return null;
     }
 
     @Redirect(method = "performCommand", at = @At(value = "INVOKE", target = "Lnet/minecraft/commands/Commands;performPrefixedCommand(Lnet/minecraft/commands/CommandSourceStack;Ljava/lang/String;)V"))
-    private void performParsed(Commands instance, CommandSourceStack commandSourceStack, String string) {
+    private void performParsed(Commands instance, CommandSourceStack commandSourceStack, String string) throws Exception {
         // Parse if not done by first call
         if (commandoptimiser$parseResults == null || !string.equals(commandoptimiser$parsedCommand))
             commandoptimiser$parse();
@@ -63,13 +85,17 @@ public abstract class BaseCommandBlockMixin {
 
 
     @Unique
-    private void commandoptimiser$parse() {
+    private void commandoptimiser$parse() throws Exception {
         // Create command source stack
-        CommandSourceStack commandSourceStack = createCommandSourceStack().withCallback((bl, i) -> {
-            if (bl) {
-                ++successCount;
-            }
-        });
+        CommandSourceStack commandSourceStack;
+        try (CloseableCommandBlockSource closeableCommandBlockSource = this.createSource()) {
+            CommandSource commandSource = Objects.requireNonNullElse(closeableCommandBlockSource, CommandSource.NULL);
+            commandSourceStack = createCommandSourceStack(commandSource).withCallback((bl, i) -> {
+                if (bl) {
+                    ++successCount;
+                }
+            });
+        }
 
         // Get server
         MinecraftServer minecraftServer = getLevel().getServer();
@@ -78,7 +104,7 @@ public abstract class BaseCommandBlockMixin {
         commandoptimiser$parsedCommand = command;
 
         // Parse command
-        commandoptimiser$commandNoSlash = command.startsWith("/") ? command.substring(1) : command;
+        commandoptimiser$commandNoSlash = trimOptionalPrefix(command);
         commandoptimiser$parseResults = minecraftServer.getCommands().getDispatcher().parse(commandoptimiser$commandNoSlash, commandSourceStack);
     }
 }
